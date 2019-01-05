60a05c761ae8cfe7630b32ad7b08905de1520a89
Basil Stotz <basil.stotz@gmail.com>
2019-01-05 16:11:55 +0000

    *

--- bin/patch.sh
+++ bin/patch.sh
@@ -2,13 +2,13 @@
 
 PORT=4567
 
-SSH="ssh -i /home/pi/bootserver-patch-raspi/data/keys/id_rsa adm-bstotz@10.249.15.254 sudo "
+SSH="ssh -i ~/bootserver-patch/data/keys/id_rsa adm-bstotz@10.249.15.254 sudo "
 
     if  fping 10.249.15.254 -a >/dev/null 2>/dev/null;then
 
-       rsync -v -r -e"ssh  -i /home/pi//bootserver-patch-raspidata/keys/id_rsa" /home/pi//bootserver-patch-raspi/data/ adm-bstotz@10.249.15.254:.
+       rsync -v -r -e"ssh  -i ~/bootserver-patch/data/keys/id_rsa" ~/bootserver-patch/data/ adm-bstotz@10.249.15.254:.
 
-       $SSH sudo bin/agent.sh $PORT
+       $SSH sudo ./bin/agent.sh $PORT
     fi
     exit
     
