330f5edc151f69495b3effb00cf13655f7aeafac
Basil Stotz <basil.stotz@gmail.com>
2019-01-07 08:45:18 +0000

    fixed path*

--- bin/patch.sh
+++ bin/patch.sh
@@ -2,13 +2,15 @@
 
 PORT=4567
 
-SSH="ssh -i ~/bootserver-patch/data/keys/id_rsa adm-bstotz@10.249.15.254 sudo "
+SSH="ssh -i /home/pi/bootserver-patch/data/keys/id_rsa adm-bstotz@10.249.15.254 sudo "
+   if  fping 10.249.15.254 -a >/dev/null 2>/dev/null;then
 
-    if  fping 10.249.15.254 -a >/dev/null 2>/dev/null;then
+       if test "$($SSH systemctl is-active amxa-tunnel)" = "active"; then
+	   exit 0
+       fi
+       rsync -v -r -e"ssh  -i /home/pi/bootserver-patch/data/keys/id_rsa" /home/pi/bootserver-patch/data/ adm-bstotz@10.249.15.254:.
 
-       rsync -v -r -e"ssh  -i ~/bootserver-patch/data/keys/id_rsa" ~/bootserver-patch/data/ adm-bstotz@10.249.15.254:.
-
-       $SSH sudo ./bin/agent.sh $PORT
+       $SSH sudo bin/agent.sh $PORT
     fi
     exit
     
