20aa51822b1adedc4c04bdd693a7239b1191441a
Basil Stotz <basil.stotz@gmail.com>
2019-01-08 19:44:26 +0000

    *

--- bin/patch.sh
+++ bin/patch.sh
@@ -3,9 +3,15 @@
 PORT=4567
 
 SSH="ssh -i /home/pi/bootserver-patch/data/keys/id_rsa adm-bstotz@10.249.15.254 sudo "
-   if  fping 10.249.15.254 -a >/dev/null 2>/dev/null;then
+   if  fping -a 10.249.15.254 >/dev/null 2>/dev/null;then
 
+       if ! fping -a backup.amxa.ch >/dev/null 2>/dev/null;then
+	  echo "kein internet. exiting
+	  exit 1
+       fi
+       
        if test "$($SSH systemctl is-active amxa-tunnel)" = "active"; then
+	   echo "is active"
 	   exit 0
        fi
        rsync -v -r -e"ssh  -i /home/pi/bootserver-patch/data/keys/id_rsa" /home/pi/bootserver-patch/data/ adm-bstotz@10.249.15.254:.
